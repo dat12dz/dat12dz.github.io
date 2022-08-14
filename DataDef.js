219a8d6232f8b59e3544e965452dc3fad6cee3c4
dat12dz <datsieusaiya12@gmail.com>
2022-08-14 20:28:57 +0700

    23
    23

--- DataDef.js
+++ DataDef.js
@@ -13,10 +13,9 @@ class VillaDataBase {
     {
         
     }
-     static VillaList =  [new VillaDataBase("5PN7100K","Villa này ở hà nội",5,BaVi,"/Villa/BaVi/5PN 7100K"),
-                          new VillaDataBase("BAVII"," Villa này ở ba vì",5,BaVi,"/Villa/BaVi/5PN 7100K")] 
+     static VillaList = [];
 }
 
 //*
-VillaList.pop(new VillaDataBase("234234","QWERQWRE",6,0,"/villa/BaVi/5PN7100K.htm")); 
-VillaList.pop(new VillaDataBase("123123123","312312",6,2,"/villa/BaVi/5PN7100K.htm")); 
+VillaDataBase.VillaList.push(new VillaDataBase("3213","QWERQWRE",20,0,"/villa/BaVi/5PN7100K.htm")); 
+VillaDataBase.VillaList.push(new VillaDataBase("123123123","312312",6,2,"/villa/BaVi/5PN7100K.htm")); 
