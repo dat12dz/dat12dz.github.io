7afd4c13e061250ded9385daf65d0a4f95153665
dat12dz <datsieusaiya12@gmail.com>
2022-08-14 19:37:36 +0700

    Update DataDef.js

--- DataDef.js
+++ DataDef.js
@@ -1,5 +1,5 @@
 
-const HoaBinh = 1,BaVi = 2,LuongSon=3,TamDao=4,HaLong = 5;
+const HoaBinh = 0,BaVi = 1,LuongSon=2,TamDao=3,HaLong = 4;
 class VillaDataBase {
     constructor (VillaName, description, SoNguoi,Location,hrefLink)
     {
@@ -13,11 +13,10 @@ class VillaDataBase {
     {
         
     }
-     static VillaList =  [new VillaDataBase("5PN7100K"," Villa này ở hà nội",5,BaVi,"/Villa/BaVi/5PN 7100K"),
+     static VillaList =  [new VillaDataBase("5PN7100K","Villa này ở hà nội",5,BaVi,"/Villa/BaVi/5PN 7100K"),
                           new VillaDataBase("BAVII"," Villa này ở ba vì",5,BaVi,"/Villa/BaVi/5PN 7100K")] 
-
 }
 
-
-
-
+//*
+VillaList.pop(new VillaDataBase("234234","QWERQWRE",6,0,"/villa/BaVi/5PN7100K.htm")); 
+VillaList.pop(new VillaDataBase("123123123","312312",6,2,"/villa/BaVi/5PN7100K.htm")); 
