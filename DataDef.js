cee14ba258870d50f6006f4baaeff9a66dcfc6e5
dat12dz <datsieusaiya12@gmail.com>
2022-08-16 16:23:52 +0700

    324
    324

--- DataDef.js
+++ DataDef.js
@@ -1,13 +1,14 @@
 
 const HoaBinh = 0,BaVi = 1,LuongSon=2,TamDao=3,HaLong = 4;
 class VillaDataBase {
-    constructor (VillaName, description, SoNguoi,Location,hrefLink)
+    constructor (VillaName, description, SoNguoi,Location,hrefLink,MainPicLink)
     {
         this.VillaName = VillaName;
         this.description = description;
         this.SoNguoi = SoNguoi;
         this.Location = Location;
         this.hrefLink = hrefLink;
+        this.MainPicLink = MainPicLink;
     }   
     static Filter()
     {
@@ -17,5 +18,5 @@ class VillaDataBase {
 }
 
 //*
-VillaDataBase.VillaList.push(new VillaDataBase("3213","QWERQWRE",20,0,"/5PN7100K.htm")); 
-VillaDataBase.VillaList.push(new VillaDataBase("123123123","312312",6,2,"/villa/BaVi/5PN7100K.htm")); 
+VillaDataBase.VillaList.push(new VillaDataBase("Đạt Đậu","Tôi tên kalf đaua",6,1,"/villa/BaVi/5PN7100K.htm","/villa/BaVi/5PN7100K_files/image013.jpg")); 
+VillaDataBase.VillaList.push(new VillaDataBase("BaVi","",6,1,"/villa/BaVi/BAVII.htm","/villa/BaVi/5PN7100K_files/image014.jpg")); 
