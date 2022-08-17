6b057373e8a0293daf98622de213dd36f46646e2
dat12dz <datsieusaiya12@gmail.com>
2022-08-17 15:21:13 +0700

    3341

--- DataDef.js
+++ DataDef.js
@@ -1,5 +1,7 @@
-
+///*
 const HoaBinh = 0,BaVi = 1,LuongSon=2,TamDao=3,HaLong = 4;
+var LocationList = [];
+///*
 class VillaDataBase {
     constructor (VillaName, description, SoNguoi,Location,hrefLink,MainPicLink,GiaTien)
     {
@@ -20,5 +22,11 @@ class VillaDataBase {
 }
 
 //*
-VillaDataBase.VillaList.push(new VillaDataBase("Đạt Đậu","Tôi tên kalf đaua",6,1,"/villa/BaVi/5PN7100K.htm","/villa/BaVi/5PN7100K_files/image013.jpg",1000000)); 
+VillaDataBase.VillaList.push(new VillaDataBase("Đạt Đậu","Tôi tên kalf đaua<br>dsfdasfsffdasfafsadf",6,1,"/villa/BaVi/5PN7100K.htm","/villa/BaVi/5PN7100K_files/image013.jpg",1000000)); 
 VillaDataBase.VillaList.push(new VillaDataBase("BaVi","",6,1,"/villa/BaVi/BAVII.htm","/villa/BaVi/5PN7100K_files/image014.jpg",5000000)); 
+//* LAPK
+LocationList.push("Hoà Bình");
+LocationList.push("Ba Vì");
+LocationList.push("Lương Sơn");
+LocationList.push("Tam Đảo");
+LocationList.push("Hạ Long");
