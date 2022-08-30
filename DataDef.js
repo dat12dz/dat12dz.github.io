9306a46b64e532dcd6faadb8741584761e981c8c
dat12dz <datsieusaiya12@gmail.com>
2022-08-30 12:38:48 +0700

    asd
    asd

--- DataDef.js
+++ DataDef.js
@@ -26,13 +26,10 @@ class VillaDataBase {
 
 //*
 VillaDataBase.VillaList.push(new VillaDataBase("5PN700K","HÓT HÓT NGUYÊN CĂN RIÊNG 5PN 7100K/20ng ",6,1,"Villa/BaVi/ST700K","Villa/BaVi/ST700K/FilePicture/5PN7100K.jpg",1000000)); 
-VillaDataBase.VillaList.push(new VillaDataBase("BA VII","Vì mây không vội vã nên trời xanh 1 màu<br>Nếu anh k vội vã, mình có kịp gặp nhau",10,1,"Villa/BaVi/BAVII","Villa/BaVi/BAVII/BAVII_files/image025.jpg",4500000)); 
-
-
+VillaDataBase.VillaList.push(new VillaDataBase("BA VII","Vì mây không vội vã nên trời xanh 1 màu<br>Nếu anh k vội vã mình có kịp gặp nhau",10,1,"Villa/BaVi/BAVII","Villa/BaVi/BAVII/BAVII_files/image025.jpg",4500000)); 
 //* LAPK
-LocationList.push("Hoà Bình");//0
-LocationList.push("Ba Vì");//1
-LocationList.push("Lương Sơn");//2
-LocationList.push("Tam Đảo");//3
-LocationList.push("Hạ Long");//4
-
+LocationList.push("Hoà Bình");
+LocationList.push("Ba Vì");
+LocationList.push("Lương Sơn");
+LocationList.push("Tam Đảo");
+LocationList.push("Hạ Long");
