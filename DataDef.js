fda3678c1efe7f7b4ffd7d00311824a55bfea029
dat12dz <datsieusaiya12@gmail.com>
2022-08-31 12:25:32 +0700

    sa
    asd

--- DataDef.js
+++ DataDef.js
@@ -27,7 +27,6 @@ class VillaDataBase {
 //*
 VillaDataBase.VillaList.push(new VillaDataBase("5PN700K","HÓT HÓT NGUYÊN CĂN RIÊNG 5PN 7100K/20ng ",6,1,"Villa/BaVi/ST700K","Villa/BaVi/ST700K/FilePicture/5PN7100K.jpg",1000000)); 
 VillaDataBase.VillaList.push(new VillaDataBase("BA VII","Vì mây không vội vã nên trời xanh 1 màu<br>Nếu anh k vội vã mình có kịp gặp nhau",10,1,"Villa/BaVi/BAVII","Villa/BaVi/BAVII/BAVII_files/image025.jpg",4500000)); 
-VillaDataBase.VillaList.push(new VillaDataBase("Villa không xác định","",0,-1,"/Villa/BaVi/villaluongson","",1000000)); 
 //* LAPK
 LocationList.push("Hoà Bình");
 LocationList.push("Ba Vì");
