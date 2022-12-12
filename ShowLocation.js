3dacaed733c7ffe6da6212d3d746709ce230cdd1
dat12dz <73179534+dat12dz@users.noreply.github.com>
2022-12-12 17:35:09 +0700

    asdasd
    asdas

--- ShowLocation.js
+++
@@ -1,5 +0,0 @@
-var i = 0;
-LocationList.forEach(element => {
-    document.getElementById("location").innerHTML += "<a href =" +document.location.origin + "/search.html?l=" + i + ">" +element;
-    i++
-});
