739045ac761b890a56191d743080e900b7df6255
datdau <datdau@DATDAU>
2023-06-14 23:00:27 +0700

    change login perform

--- user/Login.js
+++ user/Login.js
@@ -10,23 +10,26 @@ async function Login(username,password)
         },
         body: JSON.stringify({ username,password })
     }).then((res)=>{
-      if ( res.status < 300)
-      {
+  
         document.cookie = "";
-        res.json().then(j  => {
+        res.json().then(json  => {
+          if ( res.status < 300)
+          {
             // Login sucessful
             document.cookie = "acc="+ j.cookie + "path=/; domain=" + window.location.hostname;   
             document.cookie = "username="+ username + ";path=/; domain=" + window.location.hostname; 
             console.log(document.cookie);
               window.location.href = window.location.origin + "/index";
+          }
+          else
+          {
+            if (json.status == 3) writeToPopup("Fail to login","This account has not been verified. Please check your email");
+            else
+             // Login fail
+             writeToPopup("Fail to login","Password wrong or username not found");
+          }
         });
-
-      }
-      else
-      {
-        // Login fail
-        writeToPopup("Fail to login","Password wrong or username not found");
-      }
+      
     }).catch((e)=> {
         if (e == "TypeError: Failed to fetch") writeToPopup("Fail to connect","Server is closed or unable to connect"); else
         writeToPopup("Fail to login",e);
